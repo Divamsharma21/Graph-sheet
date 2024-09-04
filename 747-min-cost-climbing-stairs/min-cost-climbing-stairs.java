283e8041e7cd0683a6d4ef0356b58e180462250e
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-04 08:17:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (99.25%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -23,21 +23,43 @@ class Solution {
     // }
     // }
 
-    // this is taqbulation
+    // this is taqbulation ->
+
+    // public int minCostClimbingStairs(int[] cost) {
+    //     int n = cost.length;
+
+    //     int[] dp = new int[n];
+    //     for (int i = 0; i < n; i++) {
+    //         if (i < 2)
+    //             dp[i] = cost[i];
+    //         else
+    //             dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+    //     }
+
+    //     return Math.min(dp[n - 1], dp[n - 2]);
+
+        
+    // }
+
+       
+//    this is space optimization ->o(n)
 
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
+        int prev=cost[0];
+        int next=cost[1];
 
-        int[] dp = new int[n];
-        for (int i = 0; i < n; i++) {
-            if (i < 2)
-                dp[i] = cost[i];
-            else
-                dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+            if (n <=2)
+                return Math.min(prev,next);
+
+   
+        for (int i = 2; i < n; i++) {
+ 
+           int curr = cost[i] + Math.min(prev,next);
+           prev=next;
+           next=curr;
         }
 
-        return Math.min(dp[n - 1], dp[n - 2]);
-
-        
+        return Math.min(prev,next);
     }
 }
