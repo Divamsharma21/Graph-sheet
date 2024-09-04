a47cff2b445e9d17427f94bfe92a4f30814181cf
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-04 09:12:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (35.12%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -20,22 +20,43 @@ class Solution {
 
     // this is tabulation solution
 
-    public int rob(int[] nums) {
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+
+    //     if (n == 1) {
+    //         return nums[0];
+    //     }
+    //     int[] dp = new int[n + 1];
+    //     dp[0] = nums[0];
+    //     dp[1] = Math.max(nums[0], nums[1]);
+
+    //     for (int i = 2; i < n; i++) {
+
+    //         dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    //     }
+
+    //     return dp[n - 1];
+
+    // }
+
+    // this is space optimization
+
+
+     public int rob(int[] nums) {
         int n = nums.length;
+ 
+        int prev = nums[0];
+        
+           int prev2=0;
 
-        if (n == 1) {
-            return nums[0];
+        for (int i = 1; i < n; i++) {
+
+            int curr= Math.max(prev, prev2 + nums[i]);
+            prev2=prev;
+            prev=curr;
         }
-        int[] dp = new int[n + 1];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
 
-        for (int i = 2; i < n; i++) {
-
-            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-        }
-
-        return dp[n - 1];
+        return prev;
 
     }
 }
