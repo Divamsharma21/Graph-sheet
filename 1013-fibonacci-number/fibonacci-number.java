5fd4e00c21d2b35ba828d637d79d862bb18b72a3
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-08 16:52:12 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (51.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,8 +1,29 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
+        //  if (n == 0)
+        //     return 0;
+        // if (n == 1)
+        //     return 1;
 
-        return fib(n-1)+ fib(n-2);
-            }
+        // return fib(n - 1) + fib(n - 2);
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return memo(n,dp);
+    }
+    public int memo(int n,int[] dp){
+         if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+            if(dp[n]!=-1) return dp[n];
+
+            dp[n]=memo(n - 1,dp) + memo(n - 2,dp);
+
+            int ans=dp[n];
+
+            return ans;
+    }
+
 }
