958f3ad1248fd533f2e2a158afaa994cfa087f95
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-08 17:04:14 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (92.62%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -6,24 +6,48 @@ class Solution {
         //     return 1;
 
         // return fib(n - 1) + fib(n - 2);
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
+        // int[] dp=new int[n+1];
+        // Arrays.fill(dp,-1);
 
-        return memo(n,dp);
+        // return memo(n,dp);
+
+        // this is tabulation
+        // int[] dp=new int[n+1];
+        // dp[0]=0;
+        // dp[1]=1;
+
+        // for(int i=2;i<=n;i++){
+        //     dp[i]=dp[i-1]+dp[i-2];
+
+        // }
+
+        // int ans=dp[n];
+
+        // return ans;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int prev=1;
+        int next=1;
+        for(int  i=3;i<=n;i++){
+            int curr=prev+next;
+            prev=next;
+            next=curr;
+        }
+        return next;                        
     }
-    public int memo(int n,int[] dp){
-         if (n == 0)
-            return 0;
-        if (n == 1)
-            return 1;
+    // public int memo(int n,int[] dp){
+    //      if (n == 0)
+    //         return 0;
+    //     if (n == 1)
+    //         return 1;
 
-            if(dp[n]!=-1) return dp[n];
+    //         if(dp[n]!=-1) return dp[n];
 
-            dp[n]=memo(n - 1,dp) + memo(n - 2,dp);
+    //         dp[n]=memo(n - 1,dp) + memo(n - 2,dp);
 
-            int ans=dp[n];
+    //         int ans=dp[n];
 
-            return ans;
-    }
+    //         return ans;
+    // }
 
 }
