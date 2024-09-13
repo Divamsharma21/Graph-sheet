b3601465d6b500792cec0c8bea1bce84a4e8c2ab
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-13 14:32:34 +0530

    Time: 5 ms (66.22%) | Memory: 45 MB (34.49%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -22,49 +22,79 @@ class Solution {
     //     }
 
     //     return ans;
-    public int minDistance(String word1, String word2) {
+
+    // this is memo soltuion
+
+
+    // public int minDistance(String word1, String word2) {
+    //     int n = word1.length();
+    //    int m = word2.length();
+    //    int[][] dp = new int[n][m];
+    //    for(int[] row:dp){
+    //        Arrays.fill(row,-1);
+    //    } 
+    //     return distance(word1,word2,0,0,dp);
+    // }
+
+    // public int distance(String w1,String w2,int i,int j,int dp[][]){
+    //     // base condition 
+    //     if(i==w1.length()){
+    //         return w2.length()-j;
+    //     }
+
+    //     if(j==w2.length()){
+    //         return w1.length()-i;
+    //     }
+
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+
+    //     int findans=0;
+     
+    //  if(w1.charAt(i)==w2.charAt(j)){
+    //         return distance(w1,w2,i+1,j+1,dp);
+
+    //     }
+    //     else{
+    //         //insert
+    //         int insert=1+distance(w1,w2,i,j+1,dp);
+
+    //         // delete
+    //         int delete=1+distance(w1,w2,i+1,j,dp);
+    //         //replace
+    //         int replace=1+distance(w1,w2,i+1,j+1,dp);
+
+    //      findans=Math.min(insert,Math.min(delete,replace));
+
+    //     }
+     
+    //     return dp[i][j]=findans;
+
+
+    // this is tabulation
+
+     public int minDistance(String word1, String word2) {
         int n = word1.length();
-       int m = word2.length();
-       int[][] dp = new int[n][m];
-       for(int[] row:dp){
-           Arrays.fill(row,-1);
-       } 
-        return distance(word1,word2,0,0,dp);
-    }
-
-    public int distance(String w1,String w2,int i,int j,int dp[][]){
-        // base condition 
-        if(i==w1.length()){
-            return w2.length()-j;
+        int m = word2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=m; j++){
+                if(i==0){
+                    dp[0][j] = j;
+                    continue;
+                }
+                if(j==0){
+                    dp[i][0] = i;
+                    continue;
+                }
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = 1+Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]));
+                }
+            }
         }
-
-        if(j==w2.length()){
-            return w1.length()-i;
-        }
-
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-        int findans=0;
-     
-     if(w1.charAt(i)==w2.charAt(j)){
-            return distance(w1,w2,i+1,j+1,dp);
-
-        }
-        else{
-            //insert
-            int insert=1+distance(w1,w2,i,j+1,dp);
-
-            // delete
-            int delete=1+distance(w1,w2,i+1,j,dp);
-            //replace
-            int replace=1+distance(w1,w2,i+1,j+1,dp);
-
-         findans=Math.min(insert,Math.min(delete,replace));
-
-        }
-     
-        return dp[i][j]=findans;
+        return dp[n][m];
     }
 }
