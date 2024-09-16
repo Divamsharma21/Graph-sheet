bbf26af5419ba38714ad9de8b2a04e3cf8bf1577
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-16 15:17:27 +0530

    Time: 5 ms (70.17%) | Memory: 40.6 MB (98.78%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,37 +1,94 @@
+// class Solution {
+
+    // int[][] dp;
+
+    // public int change(int amount, int[] coins) {
+    // int n = coins.length;
+    // dp = new int[n][amount+ 1];
+    // for(int[] row:dp)
+    // {
+    // Arrays.fill(row,-1);
+    // }
+
+    // return solve(amount, coins, coins.length - 1);
+    // }
+
+    // public int solve(int amount, int[] coins, int idx) {
+
+    // if (amount == 0) {
+
+    // return 1;
+    // }
+    // if (idx < 0 || amount < 0){
+
+    // return 0;
+    // }
+
+    // if (dp[idx][amount]!= -1){
+
+    // return dp[idx][amount];
+    // }
+
+    // int take = solve(amount - coins[idx], coins, idx);
+    // int nottake = solve(amount, coins, idx - 1);
+
+    // return dp[idx][amount] = take + nottake;
+
+//     //Tabulation
+// class Solution {
+//     public int change(int T,int arr[]) {
+//        int n = arr.length;
+//         int dp[][]=new int[n][T+1];
+//     //Initializing base condition
+//     for(int i=0;i<=T;i++){
+//         if(i%arr[0]==0)
+//             dp[0][i]=1;
+//         // Else condition is automatically fulfilled,
+//         // as dp array is initialized to zero
+//     }
+//     for(int ind=1; ind<n;ind++){
+//         for(int target=0;target<=T;target++){
+//             int notTaken = dp[ind-1][target];
+            
+//             int taken = 0;
+//             if(arr[ind]<=target)
+//                 taken = dp[ind][target-arr[ind]];
+                
+//             dp[ind][target] = notTaken + taken;
+//         }
+//     }
+    
+//     return dp[n-1][T];
+//     }
+// }
+ 
+ 
+
+ //Space optimized Tabulation
 class Solution {
-
-    int[][] dp;
-
-    public int change(int amount, int[] coins) {
-        int n = coins.length;
-        dp = new int[n][amount+ 1];
-      for(int[] row:dp)
-       {
-           Arrays.fill(row,-1);
-       }
-
-        return solve(amount, coins, coins.length - 1);
+    public int change(int T,int arr[]) {
+       int n = arr.length;
+        // int dp[][]=new int[n][T+1];
+    int prev[] = new int[T+1];
+        //Initializing base condition
+    for(int i=0;i<=T;i++){
+        if(i%arr[0]==0)
+            prev[i]=1;
+        // Else condition is automatically fulfilled,
+        // as dp array is initialized to zero
     }
-
-    public int solve(int amount, int[] coins, int idx) {
-
-        if (amount == 0) {
-
-            return 1;
+    for(int ind=1; ind<n;ind++){
+        for(int target=0;target<=T;target++){
+            int notTaken = prev[target];
+            
+            int taken = 0;
+            if(arr[ind]<=target)
+                taken = prev[target-arr[ind]];
+                
+            prev[target] = notTaken + taken;
         }
-        if (idx < 0 || amount < 0){
-
-            return 0;
-        }
-
-        if (dp[idx][amount]!= -1){
-
-            return dp[idx][amount];
-        }
-
-        int take = solve(amount - coins[idx], coins, idx);
-        int nottake = solve(amount, coins, idx - 1);
-
-        return dp[idx][amount] = take + nottake;
+    }
+    
+    return prev[T];
     }
 }
