1079a4b0c55aaa7e4ebb04c2248d17acb139fb76
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-16 08:17:54 +0530

    Time: 1 ms (70.54%) | Memory: 40.8 MB (56.67%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -1,27 +1,50 @@
 class Solution {
+    // public int combinationSum4(int[] nums, int target) {
+
+    // int n = nums.length;
+    // int[] dp = new int[target + 1];
+    // Arrays.fill(dp, -1);
+
+    // return solve(nums, target, dp);
+    // }
+
+    // public int solve(int[] nums, int target, int[] dp) {
+    // int ans = 0;
+    // if (target < 0)
+    // return 0;
+    // if (target == 0)
+    // return 1;
+
+    // if (dp[target] != -1)
+    // return dp[target];
+    // for (int i = 0; i <nums.length; i++) {
+    // ans += solve(nums, target - nums[i],dp);
+
+    // }
+
+    // return dp[target] = ans;
+
     public int combinationSum4(int[] nums, int target) {
 
         int n = nums.length;
         int[] dp = new int[target + 1];
-        Arrays.fill(dp, -1);
 
-        return solve(nums, target, dp);
-    }
+        // if (dp[target] < 0)
+        // return 0;
+        // if (dp[target] == 0)
+        // return 1;
+        dp[0] = 1;
 
-    public int solve(int[] nums, int target, int[] dp) {
-        int ans = 0;
-        if (target < 0)
-            return 0;
-        if (target == 0)
-            return 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    dp[i] = dp[i] + dp[i - nums[j]];
 
-        if (dp[target] != -1)
-            return dp[target];
-        for (int i = 0; i <nums.length; i++) {
-            ans += solve(nums, target - nums[i],dp);
+                }
+            }
 
         }
 
-        return dp[target] = ans;
+        return dp[target];
     }
 }
