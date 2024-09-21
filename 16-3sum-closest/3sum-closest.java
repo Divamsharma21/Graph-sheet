7a99ddca9cc9ea6cd62cdb35b8e076be9cd0b47d
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-21 17:34:13 +0530

    Time: 742 ms (5.02%) | Memory: 43.1 MB (42.18%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,14 +1,14 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int n=nums.length;
-          
-        int closestSum = nums[0] + nums[1] + nums[2]; 
-           int sum =0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                     sum = (nums[i]+nums[j]+nums[k]);
-                      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        int n = nums.length;
+
+        int closestSum = nums[0] + nums[1] + nums[2];
+        int sum = 0;
+        for (int i = 0; i < n-2; i++) {
+            for (int j = i + 1; j < n-1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    sum = (nums[i] + nums[j] + nums[k]);
+                    if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
                         closestSum = sum;
                     }
                 }
@@ -16,16 +16,16 @@ class Solution {
         }
         return closestSum;
 
-        //  for(int i=0; i<len-2; i++){
-        //     for(int j=i+1; j<len-1; j++){
-        //         for(int k=j+1; k<len;  k++){
-        //             sum = (nums[i]+nums[j]+nums[k]);
-                     
-        //              if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-        //                 closestSum = sum;
-        //             }
-        //         }
-        //     }
+        // for(int i=0; i<len-2; i++){
+        // for(int j=i+1; j<len-1; j++){
+        // for(int k=j+1; k<len; k++){
+        // sum = (nums[i]+nums[j]+nums[k]);
+
+        // if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        // closestSum = sum;
+        // }
+        // }
+        // }
         // }
         // return closestSum;
     }
