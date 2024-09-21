03fe7d97e43e1e7c7b7d7782d8a1442c7a8f742a
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-21 17:41:47 +0530

    Time: 15 ms (47.72%) | Memory: 43.4 MB (21.72%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,33 +1,24 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int n = nums.length;
-
-        // int closestSum = nums[0] + nums[1] + nums[2];
-        int   closestSum= Integer.MAX_VALUE / 2;
-        int sum = 0;
-        for (int i = 0; i < n-2; i++) {
-            for (int j = i + 1; j < n-1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    sum = (nums[i] + nums[j] + nums[k]);
-                    if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-                        closestSum = sum;
-                    }
+         Arrays.sort(nums);
+        int n=nums.length;
+        int sum=nums[0]+nums[1]+nums[n-1];
+        for(int i=0;i<n-2;i++){
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int curr_sum=nums[i]+nums[j]+nums[k];
+                if(curr_sum<=target){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+                if(Math.abs(sum-target)>Math.abs(curr_sum-target)){
+                    sum=curr_sum;
                 }
             }
         }
-        return closestSum;
-
-        // for(int i=0; i<len-2; i++){
-        // for(int j=i+1; j<len-1; j++){
-        // for(int k=j+1; k<len; k++){
-        // sum = (nums[i]+nums[j]+nums[k]);
-
-        // if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-        // closestSum = sum;
-        // }
-        // }
-        // }
-        // }
-        // return closestSum;
+        return sum;
     }
 }
