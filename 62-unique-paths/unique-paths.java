de40a64093b422869567d8e98166d0380c97d742
DIVAM SHARMA <103370650+Divamsharma21@users.noreply.github.com>
2024-09-26 21:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (49.22%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,23 +1,28 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-          int[][] dp = new int[m][n];
-        
-        // Initialize the rightmost column and bottom row to 1
-        for (int i = 0; i < m; i++) {
-            dp[i][n-1] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            dp[m-1][j] = 1;
-        }
-        
-        // Fill in the DP array bottom-up
-        for (int i = m - 2; i >= 0; i--) {
-            for (int j = n - 2; j >= 0; j--) {
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+        int[][] dp = new int[m][n];
+
+        // if (m == 0 || n == 0)
+        // return 0;
+        if (m == 1 || n == 1)
+            return 1;
+
+        // int down=uniquePaths(m-1,n);
+        // int right=uniquePaths(m,n-1);
+
+        // return down+right;
+        for (int i = 0; i < n; i++)
+            dp[0][i] = 1;
+        for (int j = 0; j < m; j++)
+            dp[j][0] = 1;
+            
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
             }
         }
-        
-        // Return the result stored in the top-left corner
-        return dp[0][0];
+
+        return dp[m - 1][n - 1];
     }
 }
